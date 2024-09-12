ec5457eed28e2993eba15f723f8b78d0c88ffa52
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-09-12 21:39:49 +0900

    set : fastAPI, MySQL 도커구축

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app import models
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize database
+    models.SQLModel.metadata.create_all(models.engine)
+    yield
+    # Cleanup code (if any) goes here
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost",
--- app/models.py
+++ app/models.py
@@ -1,12 +1,16 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel, create_engine
+import os
 
-class Used(SQLModel, table=True):
+class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str
     password: str
 
-engine = create_engine("sqlite:///database.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+print(f"Connecting to: {DATABASE_URL}")
+
+engine = create_engine(DATABASE_URL, echo=True)
 
 SQLModel.metadata.create_all(engine)
