65e95a3a7620c601a63f184bc3f5c20481e609a1
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-09-19 16:51:08 +0900

    feat: 반환값 수정

--- app/configs.py
+++ app/configs.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get("LOCAL_DATABASE_URL")  # 서버 로컬로 실행할 시의 DB 연동 URL
-# DATABASE_URL = os.environ.get("DATABASE_URL")  
+# DATABASE_URL = os.environ.get("LOCAL_DATABASE_URL")  # 서버 로컬로 실행할 시의 DB 연동 URL
+DATABASE_URL = os.environ.get("DATABASE_URL")  
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM')
 JWT_ACCESS_EXPIRE_MINUTES = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRE_MINUTES'))
